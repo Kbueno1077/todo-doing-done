5402b30c388c3561cfc0673213c8dca18e9e3125
Kbueno1077 <kbueno1077@gmail.com>
2024-08-30 18:00:17 -0400

    Test overflow Hero Section Mobile

--- sections/Home/Features.tsx
+++ sections/Home/Features.tsx
@@ -1,3 +1,4 @@
+import IpadCursorBlockWrapper from "@/components/IpadCursorWrapper/IpadCursorWrapper";
 import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
 import React from "react";
 
@@ -5,11 +6,13 @@ function Features() {
     return (
         <section className="bg-base-200 py-12 md:py-24 lg:py-32">
             <div className="container mx-auto px-4 md:px-6">
-                <h2 className="text-3xl font-bold text-center mb-12">
-                    Key Features
-                </h2>
+                <IpadCursorBlockWrapper type="text">
+                    <h2 className="text-3xl font-bold text-center mb-12">
+                        Key Features
+                    </h2>
 
-                <InfiniteMovingCards direction="right" speed="normal" />
+                    <InfiniteMovingCards direction="right" speed="normal" />
+                </IpadCursorBlockWrapper>
             </div>
         </section>
     );
