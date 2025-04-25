f6280a6f1c1b0fbf8e98bea1050eea4665bd6369
kbueno1077 <kbueno1077@gmail.com>
2025-04-24 20:53:05 -0400

    Refactor page components for consistency and readability; remove unused dashboard page

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,11 +1,12 @@
+"use client";
 import TicketsDashboard from "@/sections/TicketsDashboard/TicketsDashboard";
 
 export default async function DemoPage() {
-    return (
-        <div className="flex-1 w-full flex flex-col">
-            <main className="flex-1 flex flex-col gap-6 px-6 py-6">
-                <TicketsDashboard />
-            </main>
-        </div>
-    );
+  return (
+    <div className="flex-1 w-full flex flex-col">
+      <main className="flex-1 flex flex-col gap-6 px-6 py-6">
+        <TicketsDashboard />
+      </main>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import Home from "@/sections/Home/Home";
 
 export default async function Index() {
-    return (
-        <div className="w-full">
-            <Home />
-        </div>
-    );
+  return (
+    <div className="w-full">
+      <Home />
+    </div>
+  );
 }
